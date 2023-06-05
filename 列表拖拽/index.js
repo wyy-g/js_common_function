ce1f485f1c0bb13fead991a4727c79545515f726
wyychipi <1482581329@qq.com>
2023-06-05 12:12:24 +0800

    列表拖拽

--- 列表拖拽/index.js
+++ 列表拖拽/index.js
@@ -11,7 +11,9 @@ const listData = [
 ]
 
     ; ((doc) => {
+        const oDragWarp1 = doc.querySelector('.drag-warp1')
         const oDragList = doc.querySelector('.drag-list')
+        const oDragList1 = doc.querySelector('.drag-list1')
 
         const init = () => {
             createElememntLi()
@@ -30,7 +32,11 @@ const listData = [
 
         function bindEvent() {
             const liItem = doc.querySelectorAll('.drag-item')
-            oDragList.addEventListener('dragover', handleDragOver)  //ondragover  被拖拽元素在目标元素上移动
+            oDragList.addEventListener('dragover', handleDragOver) //ondragover  被拖拽元素在目标元素上移动
+            oDragList1.addEventListener('dragenter', handleDargEnter) //dragenter 被拖拽元素进入目标元素的事件
+            oDragList1.addEventListener('dragleave', handleDargLeave)
+            oDragList1.addEventListener('drop', handleDrop)
+            oDragList1.addEventListener('dragover', handleDragOver1)
             liItem.forEach(item => {
                 item.addEventListener('dragstart', handleDargStart)
                 item.addEventListener('dragend', handleDargEnd)
@@ -43,7 +49,6 @@ const listData = [
             const dragingItem = oDragList.querySelector('.dragging')
             const sibItems = oDragList.querySelectorAll('.drag-item:not(.dragging)')
             const sibItem = [...sibItems].find(item => {
-                console.log(item.offsetTop)
                 return e.clientY < item.offsetTop + item.offsetHeight / 2
             })
             oDragList.insertBefore(dragingItem, sibItem)
@@ -57,6 +62,30 @@ const listData = [
 
         function handleDargEnd(e) {
             e.target.classList.remove('dragging')
+        }
+
+        function handleDargEnter(e) {
+            e.preventDefault()
+            if (e.clientX > oDragList1.offsetLeft) {
+                oDragWarp1.style.border = '1px dashed red'
+            }
+        }
+
+        function handleDargLeave(e) {
+            e.preventDefault()
+            oDragWarp1.style.border = '1px dashed #ccc'
+        }
+
+        function handleDrop(e) {
+            e.preventDefault()
+            const dragingItem = oDragList.querySelector('.dragging')
+            oDragList1.appendChild(dragingItem)
+            dragingItem.draggable = false
+            oDragWarp1.style.border = '1px dashed #ccc'
+        }
+
+        function handleDragOver1(e) {
+            e.preventDefault()
         }
 
         init()
